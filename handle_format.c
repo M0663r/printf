33c2104662ea13e5910a909ec89a445c9d35b9aa
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-07-29 23:27:59 -0700

    #

--- handle_format.c
+++ handle_format.c
@@ -22,6 +22,16 @@ int handle_format(const char *format, va_list args)
 				printed_chars += print_string(va_arg(args, char *));
 			else if (format[i] == 'd' || format[i] == 'i')
 				printed_chars += print_number(va_arg(args, int));
+			else if (format[i] == 'u')
+				printed_chars += print_unsigned(va_arg(args, unsigned int));
+			else if (format[i] == 'o')
+				printed_chars += print_octal(va_arg(args, unsigned int));
+			else if (format[i] == 'x')
+				printed_chars += print_hex(va_arg(args, unsigned int), 0);
+			else if (format[i] == 'X')
+				printed_chars += print_hex(va_arg(args, unsigned int), 1);
+			else if (format[i] == 'b')
+				printed_chars += print_binary(va_arg(args, unsigned int));
 			else if (format[i] == '%')
 				printed_chars += write(1, &format[i], 1);
 		}
